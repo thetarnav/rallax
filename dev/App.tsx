f5992de34eb4a6e9fbb19d877f5e4b86c8995711
Damian Tarnawski <gthetarnav@gmail.com>
2022-07-29 12:56:10 +0200

    add /dev playground

---
+++ dev/App.tsx
@@ -0,0 +1,7 @@
+import { Component } from "solid-js"
+
+const App: Component = () => {
+  return <div>Hello World</div>
+}
+
+export default App
---
+++ dev/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "solid-js/web"
+import "uno.css"
+
+import App from "./App"
+
+render(() => <App />, document.getElementById("root")!)
