ccc62027314fb7174d7a1a1e51b964b197143f98
Damian Tarnawski <gthetarnav@gmail.com>
2022-07-30 22:40:47 +0200

    correct build (0.0.3)

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsup"
 
-const defaultConfig = defineConfig({
+export default defineConfig({
   clean: true,
   dts: "src/index.ts",
   format: ["esm", "cjs"],
-  entryPoints: ["src/index.ts"]
-});
-export default defaultConfig;
-
-export const doubleEntryConfig = defineConfig({
-  ...defaultConfig,
-  entryPoints: ["src/index.ts", "src/server.ts"]
-});
+  entryPoints: ["src/index.ts"],
+})
